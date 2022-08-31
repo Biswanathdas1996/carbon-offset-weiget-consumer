906e776857d046d43df23eccdcda3c2b04f53fb1
Biswanath Das <cv.biswanath@gmail.com>
2022-08-31 20:33:19 +0530

    ok

--- src/Pages/SamplePage1.js
+++ src/Pages/SamplePage1.js
@@ -33,71 +33,6 @@ const displayRazorpayForWiget = async (price, purchasetransction, title) => {
   const paymentObject = new window.Razorpay(options);
   paymentObject.open();
 };
-function Header({ itemCount }) {
-  return (
-    <header className="header-wrapper-indigo">
-      <span className="count">{itemCount} tickets selected</span>
-    </header>
-  );
-}
-
-function ProductList({ products, onChangeProductQuantity, onRemoveProduct }) {
-  return (
-    <section className="container">
-      <ul className="products">
-        {products.map((product, index) => {
-          return (
-            <li className="row" key={index}>
-              <div className="col left">
-                <div className="thumbnail">
-                  <img src={product.image} alt={product.name} />
-                </div>
-                <div className="detail">
-                  <div style={{ display: "flex", flexDirection: "row" }}>
-                    <div className="name" style={{ marginRight: 20 }}>
-                      {product.from}
-                    </div>
-                    <FlightIcon style={{ marginRight: 20 }} />
-                    <div className="name">{product.to}</div>
-                  </div>
-
-                  <div className="description">{product.description}</div>
-                  <div className="price">{formatCurrency(product.price)}</div>
-                </div>
-              </div>
-
-              <div className="col right">
-                <div className="quantity">
-                  <input
-                    type="text"
-                    className="quantity"
-                    step="1"
-                    value={product.quantity}
-                    onChange={(event) => onChangeProductQuantity(index, event)}
-                  />
-                </div>
-
-                <div className="remove">
-                  <svg
-                    onClick={() => onRemoveProduct(index)}
-                    version="1.1"
-                    className="close"
-                    x="0px"
-                    y="0px"
-                    viewBox="0 0 60 60"
-                    enableBackground="new 0 0 60 60"
-                  >
-                    <polygon points="38.936,23.561 36.814,21.439 30.562,27.691 24.311,21.439 22.189,23.561 28.441,29.812 22.189,36.064 24.311,38.186 30.562,31.934 36.814,38.186 38.936,36.064 32.684,29.812" />
-                  </svg>
-                </div>
-              </div>
-            </li>
-          );
-        })}
-      </ul>
-    </section>
-  );
-}
 
 function Summary({
   subTotal,
@@ -130,12 +65,10 @@ function Summary({
         handleToggle();
         var myHeaders = new Headers();
         myHeaders.append("Content-Type", "application/json");
-
         var raw = JSON.stringify({
           product_quantity: productQty,
           wallet_address: walletAddress,
         });
-
         var requestOptions = {
           method: "POST",
           headers: myHeaders,
@@ -341,16 +274,69 @@ export default function Page() {
 
   return (
     <div>
-      <Header itemCount={itemCount} />
+      <header className="header-wrapper-indigo">
+        <span className="count">{itemCount} tickets selected</span>
+      </header>
 
       {products.length > 0 ? (
         <div>
-          <ProductList
-            products={products}
-            onChangeProductQuantity={onChangeProductQuantity}
-            onRemoveProduct={onRemoveProduct}
-          />
+          <section className="container">
+            <ul className="products">
+              {products.map((product, index) => {
+                return (
+                  <li className="row" key={index}>
+                    <div className="col left">
+                      <div className="thumbnail">
+                        <img src={product.image} alt={product.name} />
+                      </div>
+                      <div className="detail">
+                        <div style={{ display: "flex", flexDirection: "row" }}>
+                          <div className="name" style={{ marginRight: 20 }}>
+                            {product.from}
+                          </div>
+                          <FlightIcon style={{ marginRight: 20 }} />
+                          <div className="name">{product.to}</div>
+                        </div>
 
+                        <div className="description">{product.description}</div>
+                        <div className="price">
+                          {formatCurrency(product.price)}
+                        </div>
+                      </div>
+                    </div>
+
+                    <div className="col right">
+                      <div className="quantity">
+                        <input
+                          type="text"
+                          className="quantity"
+                          step="1"
+                          value={product.quantity}
+                          onChange={(event) =>
+                            onChangeProductQuantity(index, event)
+                          }
+                        />
+                      </div>
+
+                      <div className="remove">
+                        <svg
+                          onClick={() => onRemoveProduct(index)}
+                          version="1.1"
+                          className="close"
+                          x="0px"
+                          y="0px"
+                          viewBox="0 0 60 60"
+                          enableBackground="new 0 0 60 60"
+                        >
+                          <polygon points="38.936,23.561 36.814,21.439 30.562,27.691 24.311,21.439 22.189,23.561 28.441,29.812 22.189,36.064 24.311,38.186 30.562,31.934 36.814,38.186 38.936,36.064 32.684,29.812" />
+                        </svg>
+                      </div>
+                    </div>
+                  </li>
+                );
+              })}
+            </ul>
+          </section>
           <Summary
             products={products}
             subTotal={subTotal}
